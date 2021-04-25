b860479b83acd481df64ab8233d6fe407bc147c3
Argtum <silwar40k@gmail.com>
2021-04-25 21:58:09 +0300

    Рефакторинг главного компонента и компонента создающего списки

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,9 +4,7 @@ import Header from '../header';
 import RandomPlanet from '../random-planet';
 import './app.css';
 import SwapiService from "../../services/swapi-service";
-import Row from "../row";
 import ErrorBoundry from "../error-boundry";
-import ItemDetails, { Record } from "../item-details/item-details";
 import {PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList} from "../sw-components";
 
 export default class App extends Component {
@@ -22,34 +20,6 @@ export default class App extends Component {
   }
 
   render() {
-    const {getPerson, getStarship, getPersonImage, getStarshipImage} = this.swapiService;
-
-    /*const personDetails = (
-      <ItemDetails
-        itemId={11}
-        getData={getPerson}
-        getImageUrl={getPersonImage}>
-
-        <Record field="gender" label="Gender" />
-        <Record field="eyeColor" label="Eye Color" />
-        <Record field="birthYear" label="Birth Year" />
-
-      </ItemDetails>
-    );
-
-    const starshipDetails = (
-      <ItemDetails
-        itemId={5}
-        getData={getStarship}
-        getImageUrl={getStarshipImage}>
-
-        <Record field="model" label="Model" />
-        <Record field="length" label="Length" />
-        <Record field="costInCredits" label="Cost" />
-
-      </ItemDetails>
-    );*/
-
     return (
       <ErrorBoundry>
         <div className="app-container">
@@ -60,20 +30,9 @@ export default class App extends Component {
           <PlanetDetails itemId={5} />
           <StarshipDetails itemId={9} />
 
-          <PersonList>
-            {({name}) => <span>{name}</span>}
-          </PersonList>
-          <PlanetList>
-            {({name}) => <span>{name}</span>}
-          </PlanetList>
-          <StarshipList>
-            {({name}) => <span>{name}</span>}
-          </StarshipList>
-
-          {/*<Row
-            left={personDetails}
-            right={starshipDetails}
-          />*/}
+          <PersonList />
+          <PlanetList />
+          <StarshipList />
 
         </div>
       </ErrorBoundry>
