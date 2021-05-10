4737f2c7cb51e767f1457eee2751764e8713b4db
Argtum <silwar40k@gmail.com>
2021-05-10 20:24:29 +0300

    Рефакторинг - подготовка к переключеннию стрпниц

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -2,13 +2,12 @@ import React, {Component} from 'react';
 
 import Header from '../header';
 import RandomPlanet from '../random-planet';
-import SwapiService from "../../services/swapi-service";
-import ErrorBoundry from "../error-boundry";
-import {SwapiServiceProvider} from "../swapi-service-context"
+import SwapiService from '../../services/swapi-service';
+import ErrorBoundry from '../error-boundry';
+import {SwapiServiceProvider} from '../swapi-service-context'
+import {PeoplePage, PlanetPage, StarshipPage} from '../pages'
 
 import './app.css';
-
-import {PersonDetails, PersonList, PlanetDetails, PlanetList, StarshipDetails, StarshipList} from "../sw-components";
 
 export default class App extends Component {
 
@@ -30,13 +29,9 @@ export default class App extends Component {
             <Header />
             <RandomPlanet />
 
-            <PersonDetails itemId={11} />
-            <PlanetDetails itemId={5} />
-            <StarshipDetails itemId={9} />
-
-            <PersonList />
-            <PlanetList />
-            <StarshipList />
+            <PeoplePage />
+            <PlanetPage />
+            <StarshipPage />
 
           </div>
         </SwapiServiceProvider>
