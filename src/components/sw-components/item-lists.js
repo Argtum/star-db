fc22b47d6a0944ebd33ff0627036153b9db35a9b
Argtum <silwar40k@gmail.com>
2021-05-10 14:59:42 +0300

    Добавил новый аргумент (функцию преобразования данных swapiservice) для функции высшего поряджка и использовал её для списков

--- src/components/sw-components/item-lists.js
+++ src/components/sw-components/item-lists.js
@@ -1,15 +1,6 @@
 import React from "react";
-import SwapiService from "../../services/swapi-service";
-import {withData} from "../hoc-helper";
+import {withData, withSwapiService} from "../hoc-helper";
 import ItemList from "../item-list";
-
-const swapiServices = new SwapiService();
-
-const {
-	getAllPeople,
-	getAllPlanets,
-	getAllStarships
-} = swapiServices;
 
 const withChildFunction = (Wrapped, fn) => {
 	return (props) => {
@@ -24,9 +15,37 @@ const withChildFunction = (Wrapped, fn) => {
 const renderName = ({name}) => <span>{name}</span>;
 const renderModelAndName = ({model, name}) => <span>{name} ({model})</span>;
 
-const PersonList = withData(withChildFunction(ItemList, renderName), getAllPeople);
-const PlanetList = withData(withChildFunction(ItemList, renderName), getAllPlanets);
-const StarshipList = withData(withChildFunction(ItemList, renderModelAndName), getAllStarships);
+const mapPersonMethodsToProps = (swapiService) => {
+	return {
+		getData: swapiService.getAllPeople
+	}
+}
+
+const mapPlanetMethodsToProps = (swapiService) => {
+	return {
+		getData: swapiService.getAllPlanets
+	}
+}
+
+const mapStarshipMethodsToProps = (swapiService) => {
+	return {
+		getData: swapiService.getAllStarships
+	}
+}
+
+const PersonList = withSwapiService(
+	withData(withChildFunction(ItemList, renderName)),
+	mapPersonMethodsToProps
+);
+
+const PlanetList = withSwapiService(
+	withData(withChildFunction(ItemList, renderName)),
+	mapPlanetMethodsToProps
+);
+const StarshipList = withSwapiService(
+	withData(withChildFunction(ItemList, renderModelAndName)),
+	mapStarshipMethodsToProps
+);
 
 export {
 	PersonList,
